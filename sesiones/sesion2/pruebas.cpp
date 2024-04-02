2f88cd3fa5ceefe764097e549e208482564b80a3
ArelyX1 <andrycaceres2006@gmail.com>
2024-04-02 01:31:09 -0500

    agregando propuesto1.cpp y carpetas

--- sesiones/sesion2/pruebas.cpp
+++ sesiones/sesion2/pruebas.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
-#include <limits>
+#include <vector>
 #include <cstring>
 using namespace std;
 
-class Animales{
-    protected:
-        char nombre[20];
+void menu();
+void menu_metodos();
 
-    private:
-        string edad;
-    public:
-        Animales(string, string edad_inicial = "Desconocido");
-        virtual void hacerSonido();
-        void setNombre(char nombre_nuevo[20]){strncpy(nombre, nombre_nuevo, sizeof(nombre));}
-        char* getNombre(){return nombre;}
-        void setEdad(string edad_nuevo){this -> edad = edad_nuevo;}
-        string getEdad(){return edad;}
+class Animales {
+protected:
+    char nombre[20];
+    string edad;
+
+public:
+    Animales(string nombre_inicial, string edad_inicial = "Desconocido");
+    virtual void hacerSonido();
+    void setNombre(char nombre_nuevo[20]) { strncpy(nombre, nombre_nuevo, sizeof(nombre)); }
+    char* getNombre() { return nombre; } // Corregido para devolver un puntero a char
+    void setEdad(string edad_nuevo) { edad = edad_nuevo; }
+    string getEdad() { return edad; }
 };
-Animales::Animales(string nombre_inicial, string edad_inicial){
+
+Animales::Animales(string nombre_inicial, string edad_inicial /*= "Desconocido"*/) {
     strncpy(nombre, nombre_inicial.c_str(), sizeof(nombre));
-    this -> nombre[sizeof(nombre) - 1] = '\0';
-    this -> edad = edad_inicial;
+    nombre[sizeof(nombre) - 1] = '\0';
+    edad = edad_inicial;
 }
-void Animales :: hacerSonido(){
+
+void Animales::hacerSonido() {
     cout << "El animal hizo un sonido raro y genérico";
 }
+
 
 class Mamiferos : public Animales {
     protected:
@@ -38,8 +43,9 @@ class Mamiferos : public Animales {
         string getTipoPelo() { return tipo_pelo; }
 };
 Mamiferos::Mamiferos(string nombre_inicial, string edad_inicial, string tipo_pelo_inicial) : Animales(nombre_inicial, edad_inicial) {
-    tipo_pelo = tipo_pelo_inicial;
+    this -> tipo_pelo = tipo_pelo_inicial;
 }
+
 
 class Reptiles : public Animales {
     protected:
@@ -120,16 +126,59 @@ Canarios::Canarios(string nombre_inicial, string edad_inicial, string tipo_pluma
     this -> color = color_inicial;
 }
 
-
-int main(){
+int main() {
     char nombre[20];
-    int edad;
-    cout << "Ingrese la edad del animal: ";
-    cin >> edad;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cout << "Ingrese el nombre del animal: ";
-    cin.getline(nombre, 20);
-    Animales perro(nombre, to_string(edad));
-    cout << "El animal se llama: " << perro.getNombre() << endl;
+    do{
+        cout << "Ingrese el nombre del animal: ";
+        cin.getline(nombre, 20);
+    } while(strlen(nombre) == 0);
+    Animales perro(nombre);
     cout << perro.getEdad();
+    cout << perro.getNombre() << endl;
+    perro.hacerSonido();
+    
+    Mamiferos gato("Gato", "2", "Corto");
+    cout << gato.getNombre() << endl;
+    cout << gato.getEdad() << endl;
+    cout << gato.getTipoPelo() << endl;
+    gato.hacerSonido();
+
+    do{
+        int opcion;
+        menu();
+        cout << "Ingrese una opción: ";
+        cin >> opcion;
+        switch (opcion)
+        {
+        case 1:
+        
+            break;
+        
+        default:
+            break;
+        }
+    }while();
+}
+
+void menu(){
+    cout << "1. Crear un animal" << endl;
+    cout << "2. Crear un mamífero" << endl;
+    cout << "3. Crear un reptil" << endl;
+    cout << "4. Crear un ave" << endl;
+    cout << "5. Crear un perro" << endl;
+    cout << "6. Crear una lagartija" << endl;
+    cout << "7. Crear un canario" << endl;
+    cout << "8. Salir" << endl;
+}
+
+void menu_metodos(){
+    cout << "1. Hacer sonido" << endl;
+    cout << "2. Cambiar nombre" << endl;
+    cout << "3. Cambiar edad" << endl;
+    cout << "4. Cambiar tipo de pelo" << endl;
+    cout << "5. Cambiar tipo de escamas" << endl;
+    cout << "6. Cambiar tipo de plumaje" << endl;
+    cout << "7. Cambiar raza" << endl;
+    cout << "8. Cambiar color" << endl;
+    cout << "9. Salir" << endl;
 }
