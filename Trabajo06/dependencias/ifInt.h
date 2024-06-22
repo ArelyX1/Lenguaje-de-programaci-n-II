923d7371c08a559c599a33c3f2c48348f31cd724
ArelyX1 <119446617+ArelyX1@users.noreply.github.com>
2024-06-21 22:38:12 -0500

    Add files via upload

---
+++ Trabajo06/dependencias/ifInt.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <limits>
+using namespace std;
+
+int getInputInt(){
+    do{
+        int numero;
+        try {
+            cout << "Ingrese una opción: ----------------- ";
+            cin >> numero;
+            if(cin.fail()) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                throw invalid_argument("Entrada inválida. Por favor, intente de nuevo.");
+            }else
+            return numero;
+        } catch (const invalid_argument& ia) {
+            cerr << ia.what() << endl;
+        }
+    }while (1);
+}
